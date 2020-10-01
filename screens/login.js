1b6b552b1c39eea6bb4c0d74974fc636fdb948e8
Vinícius Martins <vinicius.martins@fcamara.com.br>
2020-09-30 22:33:35 -0300

    Melhorando perfomance dos campos de login

--- screens/login.js
+++ screens/login.js
@@ -2,16 +2,17 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 import { Title, Button, Subheading } from "react-native-paper";
 import TextInput from "../shared/componentes/TextInput";
-import { Formik } from "formik";
-import Yup from "../shared/validators/validator";
+import { useForm, Controller } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers";
+import * as yup from "yup";
 
 export default function App({ navigation }) {
   const [iconeSenha, setIconeSenha] = React.useState("eye-outline"),
     [mostrarSenha, setMostrarSenha] = React.useState(true);
 
-  const schema = Yup.object({
-    Usuario: Yup.string().required().min(3),
-    Senha: Yup.string().required().min(3),
+  const schema = yup.object().shape({
+    Usuario: yup.string().required().min(3),
+    Senha: yup.string().required().min(3),
   });
 
   function trocarTipoSenha() {
@@ -19,75 +20,76 @@ export default function App({ navigation }) {
     setIconeSenha(mostrarSenha ? "eye-off-outline" : "eye-outline");
   }
 
+  const { control, handleSubmit, errors } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onBlur",
+  });
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <Formik
-      initialValues={{ Usuario: "", Senha: "" }}
-      validationSchema={schema}
-      onSubmit={(data) => {
-        console.log("Submit: ", data);
-      }}
-      validateOnChange={false}
-      validateOnBlur={true}
-    >
-      {({
-        errors,
-        values,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        touched,
-      }) => (
-        <View style={styles.container}>
-          <Title style={{ textAlign: "center" }}>Por favor</Title>
+    <View style={styles.container}>
+      <Title style={{ textAlign: "center" }}>Por favor</Title>
+      <Controller
+        control={control}
+        render={({ onChange, onBlur, value }) => (
           <TextInput
-            value={values.Usuario}
-            error={touched.Usuario && errors.Usuario}
-            onChangeText={handleChange("Usuario")}
-            onBlur={handleBlur("Usuario")}
+            value={value}
+            error={errors.Usuario?.message}
+            onChangeText={(value) => onChange(value)}
+            onBlur={onBlur}
             nomeIcone="alert-circle-outline"
             label="Usuário"
           />
+        )}
+        name="Usuario"
+        defaultValue=""
+      />
+      <Controller
+        control={control}
+        render={({ onChange, onBlur, value }) => (
           <TextInput
-            value={values.Senha}
-            error={touched.Senha && errors.Senha}
-            mode="outlined"
+            value={value}
+            error={errors.Senha?.message}
             nomeIcone={iconeSenha}
             funcaoIcone={trocarTipoSenha}
             label="Senha"
             secureTextEntry={mostrarSenha}
-            onChangeText={handleChange("Senha")}
-            onBlur={handleBlur("Senha")}
+            onChangeText={(value) => onChange(value)}
+            onBlur={onBlur}
           />
-          <Button
-            mode="contained"
-            onPress={handleSubmit}
-            contentStyle={styles.button}
-            style={{ marginTop: 4 }}
-            labelStyle={{ color: "white" }}
-          >
-            ENTRAR
-          </Button>
-          <Subheading style={{ paddingVertical: 12, textAlign: "center" }}>
-            Não tem login? Cadastre-se abaixo
-          </Subheading>
-          <Button mode="text" onPress={() => navigation.goBack()}>
-            Lista de Empresas
-          </Button>
-          <Button
-            mode="text"
-            onPress={() => navigation.navigate("Cadastro Prestador")}
-          >
-            Cadastro de prestador de serviço
-          </Button>
-          <Button
-            mode="text"
-            onPress={() => navigation.navigate("Cadastro Cliente")}
-          >
-            Cadastro de cliente
-          </Button>
-        </View>
-      )}
-    </Formik>
+        )}
+        name="Senha"
+        defaultValue=""
+      />
+
+      <Button
+        mode="contained"
+        onPress={handleSubmit(onSubmit)}
+        contentStyle={styles.button}
+        style={{ marginTop: 4 }}
+        labelStyle={{ color: "white" }}
+      >
+        ENTRAR
+      </Button>
+      <Subheading style={{ paddingVertical: 12, textAlign: "center" }}>
+        Não tem login? Cadastre-se abaixo
+      </Subheading>
+      <Button mode="text" onPress={() => navigation.goBack()}>
+        Lista de Empresas
+      </Button>
+      <Button
+        mode="text"
+        onPress={() => navigation.navigate("Cadastro Prestador")}
+      >
+        Cadastro de prestador de serviço
+      </Button>
+      <Button
+        mode="text"
+        onPress={() => navigation.navigate("Cadastro Cliente")}
+      >
+        Cadastro de cliente
+      </Button>
+    </View>
   );
 }
 
